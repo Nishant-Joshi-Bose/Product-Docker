e0ae1c305cf70f1a598b621fc810f6c1959bb6e9
Mike Moretti <michael_moretti@bose.com>
2017-04-10 13:42:20 -0400

    Start setting up eddie app build stuff

---
+++ eddie-build/Main.cpp
@@ -0,0 +1,37 @@
+/*
+  Eddie SoundTouch product.
+
+  This is the main file of the current Product Eddie project.
+ */
+
+#include <signal.h>
+
+#include "DPrint.h"
+
+static DPrint s_logger( "Eddie" );
+
+using namespace Eddie;
+
+void SignalHandler( int signal )
+{
+    exit( signal );
+}
+
+int main( int argc, char** argv )
+{
+    DPrint::Initialize( "Eddie" );
+    signal( SIGINT,  SignalHandler );
+    signal( SIGTERM, SignalHandler );
+
+    //ControllerTask controllerTask;
+
+    //ControllerITC controllerItc( controllerTask );
+
+    //SoundTouchsdkInterface.Start();
+
+    BOSE_DEBUG( s_logger, "Eddie product v0.1" );
+
+    for( ;; );
+
+    return 0;
+}
