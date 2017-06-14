e7143985bf777b7b7d88cb37d38970c2ba7dff5e
Mike Moretti <mike@mordent.com>
2017-06-14 09:05:16 -0400

    Add new protocol command defs

--- Eddie_DP0_Slider/Slider.cydsn/comms.h
+++ Eddie_DP0_Slider/Slider.cydsn/comms.h
@@ -18,4 +18,22 @@ void CommsSendData(uint8_t count, const uint8_t *buffer);
 void CommsResetInputBuffer(void);
 void CommsHandleIncoming(void);
 
+typedef enum {
+    COMMS_COMMAND_GETVERSION = 0,
+    COMMS_COMMAND_LEDS_SETUP,
+    COMMS_COMMAND_LEDS_CLEARALL,
+    COMMS_COMMAND_LEDS_SETALL,
+    COMMS_COMMAND_LEDS_SETONE,
+    COMMS_COMMAND_SENSOR_ENABLE,
+    COMMS_COMMAND_SENSOR_DISABLE,
+    COMMS_COMMAND_BUTTONS_SETUP,
+    COMMS_COMMAND_SLIDERS_SETUP,
+} CommsCommand_t;
+
+typedef enum {
+    COMMS_RESPONSE_STATUS = 0,
+    COMMS_RESPONSE_SLIDER,
+    COMMS_RESPONSE_BUTTON,
+} CommsResponse_t;
+
 #endif /* COMMS_H_ */
