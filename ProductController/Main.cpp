7baa86ef63a11818d094cceae677a33b4ccf4503
Michael Cook <Michael_Cook@bose.com>
2017-07-24 11:35:26 -0400

    Eddie Main: Don't use Daemon.h.
    Per previous discussions with Santosh.  Eddie daemon doesn't need
    to catch signals to do anything on shutdown, we can just take the
    simpler route and let SIGTERM end the process immediately.

--- ProductController/Main.cpp
+++ ProductController/Main.cpp
@@ -4,48 +4,56 @@
   This is the main file of Eddie application process.
  */
 
-#include "Daemon.h"
 #include "DPrint.h"
 #include "BoseVersion.h"
+#include "SystemUtils.h"
+#include <unistd.h>
+#include <signal.h>
+#include <iostream>
+#include <stdexcept>
 
-static DPrint s_logger( "EddieApp" );
-
-class EddieApplication : public Daemon
+namespace
 {
 
-public:
+DPrint s_logger( "EddieApp" );
 
-    EddieApplication();
-    void PrepareToLive() override;
-    void PrepareToDie() override;
-
-public:
-
+struct EddieApp
+{
+    void Initialize();
     //ControllerTask m_controllerTask;
     //ControllerITC m_controllerItc;
     //EddieSoundTouchInterface m_SoundTouchInterface;
     //EddieCliClient m_eddieCliClient;
 };
 
-EddieApplication::EddieApplication()  : Daemon()
-{
-}
-
-void EddieApplication::PrepareToLive()
-{
-    BOSE_INFO( s_logger, "Version: " VERSION_MAJOR "." VERSION_MINOR "."
-               VERSION_PATCH "." VERSION_BUILD_NUMBER " "
-               VERSION_BUILD_DEVELOPER "@" VERSION_BUILD_MACHINE " "
-               VERSION_BUILD_TIME );
-}
-
-void EddieApplication::PrepareToDie()
+void EddieApp::Initialize()
 {
     BOSE_INFO( s_logger, __func__ );
 }
 
-std::unique_ptr<Daemon> Daemon::Factory()
-{
-    return std::unique_ptr<Daemon> { new EddieApplication{} };
-}
+} // namespace
 
+int main( int argc, char** argv )
+try
+{
+    DPrint::Initialize();
+    SystemUtils::ThereCanBeOnlyOne();
+
+    BOSE_INFO( s_logger, "Version: " VERSION_MAJOR "." VERSION_MINOR "."
+               VERSION_PATCH "." VERSION_BUILD_NUMBER " "
+               VERSION_BUILD_DEVELOPER "@" VERSION_BUILD_MACHINE " "
+               VERSION_BUILD_TIME );
+
+    ::signal( SIGPIPE, SIG_IGN );
+
+    EddieApp app;
+    app.Initialize();
+    while( true )
+        ::pause();
+}
+catch( std::exception const& exc )
+{
+    std::clog << SystemUtils::GetEXE() << ": Caught top level exception: "
+              << exc.what() << std::endl;
+    _exit( 1 );
+}
