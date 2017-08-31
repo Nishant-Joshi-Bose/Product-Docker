7f69b7d2b28154eae144fcb5a9e47b9ce2adbe17
Pratik Joshi <pj48403@centos_vm_pj48403.bose.com>
2017-08-31 17:46:58 -0400

    CASTLE-2740 fix

--- ProductController/Hsm/ProductAppStates.h
+++ ProductController/Hsm/ProductAppStates.h
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
-
+#include <map>
 // Important Note: HsmState framework reserves '0' as top state. DO NOT USE!
 
 enum ProductAppStates
@@ -20,4 +20,17 @@ enum ProductAppStates
     PRODUCT_APP_STATE_IDLE,
     PRODUCT_APP_STATE_LOW_POWER,
     PRODUCT_APP_CRITICAL_ERROR
+};
+
+static std::map <ProductAppStates , std::string> hsm_states =
+{
+    {PRODUCT_APP_STATE_BOOTING      , "BOOTING"},
+    {PRODUCT_APP_STATE_STDOP        , "STDOP"},
+    {PRODUCT_APP_STATE_SETUP        , "SETUP"},
+    {PRODUCT_APP_STATE_STANDBY      , "NETWORK_STANDBY"},
+    {PRODUCT_APP_STATE_ON           , "ON"},
+    {PRODUCT_APP_STATE_SW_UPDATING  , "UPDATE"},
+    {PRODUCT_APP_STATE_IDLE         , "IDLE"},
+    {PRODUCT_APP_STATE_LOW_POWER    , "LOWPOWER"},
+    {PRODUCT_APP_CRITICAL_ERROR     , "ERROR"}
 };
