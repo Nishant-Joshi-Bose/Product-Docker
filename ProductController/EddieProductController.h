1a182cff02a36db24b1846778e49ca7e848609b7
Ajay Khambadkone <ajay_khambadkone@bose.com>
2017-11-03 13:37:59 -0400

    Incremental changes for Bluetooth UI handling in PC

--- ProductController/EddieProductController.h
+++ ProductController/EddieProductController.h
@@ -33,6 +33,7 @@
 #include "KeyHandler.h"
 #include "IntentHandler.h"
 #include "ProductSTSController.h"
+#include "BluetoothSinkService.pb.h"
 
 namespace ProductApp
 {
@@ -296,6 +297,11 @@ public:
         return m_nowPlaying;
     }
 
+    const BluetoothSinkService::ListResponse& GetBluetoothList() const
+    {
+        return m_bluetoothList;
+    }
+
     //////////////////////////////////////////////////////////////////////////////////////////////
     ///
     /// @brief Interfaces to the ProductSTSController, which implements the interactions
@@ -329,6 +335,7 @@ private:
     ProductPb::ConfigurationStatus              m_ConfigurationStatus;
     ProductPb::Language                         m_systemLanguage;
     SoundTouchInterface::NowPlayingJson         m_nowPlaying;
+    BluetoothSinkService::ListResponse          m_bluetoothList;
     NetManager::Protobuf::NetworkStatus         m_cachedStatus;
 
     ProductCliClient                            m_productCliClient;
