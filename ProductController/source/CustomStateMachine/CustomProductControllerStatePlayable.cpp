08537804e70e4c26be99da1daa9b8bd570ae2d05
Yishai Sered <yishai_sered@bose.com>
2017-11-07 11:57:15 -0500

    Implement review comment: CustomProductControllerStatePlayable::HandleNowSelectionInfo() will go to PROFESSOR_PRODUCT_CONTROLLER_STATE_PLAYING_INACTIVE.

--- ProductController/source/CustomStateMachine/CustomProductControllerStatePlayable.cpp
+++ ProductController/source/CustomStateMachine/CustomProductControllerStatePlayable.cpp
@@ -138,12 +138,23 @@ bool CustomProductControllerStatePlayable::HandlePowerState( )
     return true;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// @brief  CustomProductControllerStatePlayable::HandleNowSelectionInfo
+///
+///
+/// @param  const SoundTouchInterface::NowSelectionInfo&
+///
+/// @return This method returns a true Boolean value indicating that it has handled the event
+///         and no futher processing will be required by any of its superstates.
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////
 bool CustomProductControllerStatePlayable::HandleNowSelectionInfo( const SoundTouchInterface::NowSelectionInfo& nowSelectionInfo )
 {
     BOSE_VERBOSE( s_logger, "%s is changing to %s.",
                   "CustomProductControllerStatePlayable",
                   "CustomProductControllerStatePlaying" );
-    ChangeState( PROFESSOR_PRODUCT_CONTROLLER_STATE_PLAYING );
+    ChangeState( PROFESSOR_PRODUCT_CONTROLLER_STATE_PLAYING_INACTIVE );
 
     return true;
 }
