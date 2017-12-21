d825752bd30cdb6bd9514de80e3aeba612bd96f0
Sisi-Sun-Bose <Sisi_Sun@bose.com>
2017-12-21 13:51:04 -0500

    pull in latest CastleAudioPath; modify create-product-ipk script to copy dsp library on board; comment out ALSA low latency channel per Dominic's suggestion

--- opt-bose-fs/bin/set_ap.sh
+++ opt-bose-fs/bin/set_ap.sh
@@ -6,13 +6,9 @@ if (( $# > 0 )); then
     exit 1
 fi
 
-function cset {
-    amixer --quiet cset "$@"
-}
-
 # Hack to work around a systemd boot ordering dependency (CASTLE-2106).
 i=0
-while ! cset name="SLIMBUS_6_RX Audio Mixer MultiMedia1" "1"; do
+while ! amixer cset name="SLIMBUS_6_RX Audio Mixer MultiMedia1" "1"; do
     if (( ( i += 1 ) == 10 )); then
         echo "$0: giving up"
         break
@@ -21,34 +17,35 @@ while ! cset name="SLIMBUS_6_RX Audio Mixer MultiMedia1" "1"; do
     sleep 1
 done
 
-cset name="SLIMBUS_6_RX Audio Mixer MultiMedia1" "1"
-cset name="SLIM RX2 MUX" "AIF4_PB"
-cset name="SLIM RX3 MUX" "AIF4_PB"
-cset name="SLIM_6_RX Channels" "Two"
-cset name="RX INT1_2 MUX" "RX2"
-cset name="RX INT2_2 MUX" "RX3"
-cset name="RX INT1 DEM MUX" "CLSH_DSM_OUT"
-cset name="RX INT2 DEM MUX" "CLSH_DSM_OUT"
-cset name='PCM_Dev 12 Topology Playback' 'LL'
+amixer cset name="SLIMBUS_6_RX Audio Mixer MultiMedia1" "1"
+amixer cset name="SLIM RX2 MUX" "AIF4_PB"
+amixer cset name="SLIM RX3 MUX" "AIF4_PB"
+amixer cset name="SLIM_6_RX Channels" "Two"
+amixer cset name="RX INT1_2 MUX" "RX2"
+amixer cset name="RX INT2_2 MUX" "RX3"
+amixer cset name="RX INT1 DEM MUX" "CLSH_DSM_OUT"
+amixer cset name="RX INT2 DEM MUX" "CLSH_DSM_OUT"
+#disable low latency channel per Dominic Arcoraci's suggestion
+#amixer cset name='PCM_Dev 12 Topology Playback' 'LL'
 
-cset name="MultiMedia1 Mixer SLIM_0_TX" "1"
-cset name="ADC2 Volume" "12"
-cset name="ADC3 Volume" "12"
-cset name="AIF1_CAP Mixer SLIM TX7" "1"
-cset name="AIF1_CAP Mixer SLIM TX8" "1"
-cset name="SLIM_0_TX Channels" "Two"
-cset name="SLIM TX7 MUX" "DEC7"
-cset name="SLIM TX8 MUX" "DEC8"
-cset name="ADC MUX7" "AMIC"
-cset name="ADC MUX8" "AMIC"
-cset name="AMIC MUX7" "ADC2"
-cset name="AMIC MUX8" "ADC3"
+amixer cset name="MultiMedia1 Mixer SLIM_0_TX" "1"
+amixer cset name="ADC2 Volume" "12"
+amixer cset name="ADC3 Volume" "12"
+amixer cset name="AIF1_CAP Mixer SLIM TX7" "1"
+amixer cset name="AIF1_CAP Mixer SLIM TX8" "1"
+amixer cset name="SLIM_0_TX Channels" "Two"
+amixer cset name="SLIM TX7 MUX" "DEC7"
+amixer cset name="SLIM TX8 MUX" "DEC8"
+amixer cset name="ADC MUX7" "AMIC"
+amixer cset name="ADC MUX8" "AMIC"
+amixer cset name="AMIC MUX7" "ADC2"
+amixer cset name="AMIC MUX8" "ADC3"
 
-cset name="SEC_TDM_RX_0 Audio Mixer MultiMedia1" 1
-cset name="SEC_TDM_RX_0 Channels" "Eight"
-cset name="MultiMedia1 Mixer SEC_TDM_TX_0" 1
-cset name="SEC_TDM_TX_0 Channels" "Eight"
+amixer cset name="SEC_TDM_RX_0 Audio Mixer MultiMedia1" 1
+amixer cset name="SEC_TDM_RX_0 Channels" "Eight"
+amixer cset name="MultiMedia1 Mixer SEC_TDM_TX_0" 1
+amixer cset name="SEC_TDM_TX_0 Channels" "Eight"
 
-cset name="MultiMedia2 Mixer PRI_TDM_TX_0" "1"
-cset name="PRI_TDM_TX_0 Channels" "Eight"
-cset name="PRI_TDM_TX_0 SampleRate" "KHZ_16"
+amixer cset name="MultiMedia2 Mixer PRI_TDM_TX_0" "1"
+amixer cset name="PRI_TDM_TX_0 Channels" "Eight"
+amixer cset name="PRI_TDM_TX_0 SampleRate" "KHZ_16"
