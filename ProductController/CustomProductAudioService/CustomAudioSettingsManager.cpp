9ec98e9a4957a99eaa2b58b9c3841e7fe5303b34
Sisi-Sun-Bose <Sisi_Sun@bose.com>
2018-01-09 11:29:00 -0500

    bring in corresponding CastleProductControllerCommon package

--- ProductController/CustomProductAudioService/CustomAudioSettingsManager.cpp
+++ ProductController/CustomProductAudioService/CustomAudioSettingsManager.cpp
@@ -65,22 +65,57 @@ const ProductPb::AudioTrebleLevel& CustomAudioSettingsManager::GetTreble() const
 void CustomAudioSettingsManager::InitializeAudioSettings()
 {
     BOSE_DEBUG( s_logger, __func__ );
-    //TODO: read from persistence, if failed or if major version doesn't match then read from Default file
-    //m_audioSettings
+    bool success = true; //successful reading from persistence, initialized to be true
     Json::Reader reader;
-    std::ifstream in( kDefaultConfigPath );
-    auto const& defaultAudioSettings = SystemUtils::ReadFile( kDefaultConfigPath );
-    if( !reader.parse( *defaultAudioSettings, m_audioSettings ) )
+    try
     {
-        BOSE_INFO( s_logger, reader.getFormattedErrorMessages().c_str() );
-        return;
+        std::string s = m_audioSettingsPersistence->Load();
+        success = reader.parse( s, m_audioSettings );
+        if( !success )
+        {
+            BOSE_DEBUG( s_logger, reader.getFormattedErrorMessages().c_str() );
+        }
+        else if( m_audioSettings.empty()
+                 || ( !m_audioSettings.isMember( "configurations" ) )
+                 || ( !m_audioSettings.isMember( "values" ) ) )
+        {
+            success = false;
+            BOSE_ERROR( s_logger, "Persisted m_audioSettings doesn't contain valid data" );
+        }
     }
-    else if( m_audioSettings["version"]["major"].asInt() != kConfigVersionMajor )
+    catch( ProtoPersistenceIF::ProtoPersistenceException& e )
     {
-        BOSE_INFO( s_logger, "DefaultAudioSettings.json has version %d.%d, whereas Eddie expects version %d.%d",
-                   m_audioSettings["version"]["major"].asInt(), m_audioSettings["version"]["minor"].asInt(),
-                   kConfigVersionMajor, kConfigVersionMinor );
+        success = false;
+        BOSE_ERROR( s_logger, "Loading audioSettings from persistence failed - %s ", e.what() );
     }
+
+    // If reading from persistence failed, read from default configuration file
+    if( !success )
+    {
+        BOSE_DEBUG( s_logger, "Reading audio settings from persistence failed, let's read from default config file" );
+        std::ifstream in( kDefaultConfigPath );
+        auto const& defaultAudioSettings = SystemUtils::ReadFile( kDefaultConfigPath );
+        if( !reader.parse( *defaultAudioSettings, m_audioSettings ) )
+        {
+            // If reading from default configuration file failed, there's something majorly wrong, have to return
+            BOSE_ERROR( s_logger, "Reading from default config file also failed with error %s", reader.getFormattedErrorMessages().c_str() );
+            return;
+        }
+        else if( m_audioSettings["version"]["major"].asInt() != kConfigVersionMajor )
+        {
+            // major version means major format or structure change in audio settings JSON
+            // If the major version doesn't match between configuration file and code, there's mismatch during build procedure
+            // force loading it will cause unknown error, should report error and stop
+            BOSE_ERROR( s_logger, "Reading from default config file also failed. DefaultAudioSettings.json has version %d.%d, and Professor expects version %d.%d, check your build system",
+                        m_audioSettings["version"]["major"].asInt(), m_audioSettings["version"]["minor"].asInt(),
+                        kConfigVersionMajor, kConfigVersionMinor );
+            return;
+        }
+    }
+
+    // If it gets here, it means we successfully read from either persistence or default config file
+    // Initialize ProtoBufs with m_audioSettings JSON values
+    BOSE_DEBUG( s_logger, "Initialize current protos with m_audioSettings" );
     initializeProto( kBassName, m_currentBass );
     initializeProto( kTrebleName, m_currentTreble );
 }
