cf55c63454ce34730e444e22f5715d709a5a56d5
Chirag Desai <34300742+Chirag-Desai-Einfochips@users.noreply.github.com>
2018-02-01 17:02:07 +0530

    Create product package with signature (#311)
    * Public keys and CA file copied from create-product-ipk script
    
    * Added private key containers for prod and dev builds
    
    * removing production private key
    
    * Updated Makefile to use appropriate private key file to generate signature based on device type
    
    * Updated create-product-ipk script to copy appropriate public key files based on device type
    
    * Condition updated for device type
    
    * Added private key password file
    
    * Changed secure directory location
    
    * Issue solved during directory creation
    
    * Update softwareupdate component
    
    * Review comments addressed
    
    * Review comments fixed

--- Settings.mk
+++ Settings.mk
@@ -10,6 +10,15 @@ else
   cfg = Release
 endif
 
+ifeq ($(DEPLOY_ENV),$(filter dev DEV, $(DEPLOY_ENV)))
+	privateKeyFilePath="$(PWD)/scripts/keys/development/privateKey/dev.p12"
+	privateKeyPasswordPath="$(PWD)/scripts/keys/development/privateKey/dev_p12.pass"
+else
+        #todo : handle production case
+	privateKeyFilePath="prod"
+	privateKeyPasswordPath="prod"
+endif
+
 jobs := $(shell grep -c ^processor /proc/cpuinfo)
 
 $(info sdk=${sdk} cfg=${cfg} jobs=${jobs})
@@ -17,3 +26,4 @@ $(info sdk=${sdk} cfg=${cfg} jobs=${jobs})
 BUILDS_DIR := $(BOSE_WORKSPACE)/builds/$(cfg)/$(sdk)
 
 PROTO_CC_DEST := $(BUILDS_DIR)/proto
+
