bc1ef015138d61e7620d58c7ebe09864c579e49b
Suhail-Ahmed-Randstad <Suhail_Ahmed@bose.com>
2018-02-21 10:34:54 -0500

    API to set wifi count, this requires while manual setup

--- ProductController/EddieProductController.cpp
+++ ProductController/EddieProductController.cpp
@@ -59,6 +59,7 @@ EddieProductController::EddieProductController( std::string const& ProductName )
     m_ProductControllerStatePlayingSelectedNotSilent( GetHsm(), &m_ProductControllerStatePlayingSelected, PRODUCT_CONTROLLER_STATE_PLAYING_SELECTED_NOT_SILENT ),
     m_ProductControllerStatePlayingSelectedSetup( GetHsm(), &m_ProductControllerStatePlayingSelected, PRODUCT_CONTROLLER_STATE_PLAYING_SELECTED_SETUP ),
     m_ProductControllerStatePlayingSelectedSetupNetwork( GetHsm(), &m_ProductControllerStatePlayingSelectedSetup, PRODUCT_CONTROLLER_STATE_PLAYING_SELECTED_SETUP_NETWORK ),
+    m_ProductControllerStatePlayingSelectedSetupNetworkTransition( GetHsm(), &m_ProductControllerStatePlayingSelectedSetupNetwork, PRODUCT_CONTROLLER_STATE_PLAYING_SELECTED_SETUP_NETWORK_TRANSITION ),
     m_ProductControllerStatePlayingSelectedSetupOther( GetHsm(), &m_ProductControllerStatePlayingSelectedSetup, PRODUCT_CONTROLLER_STATE_PLAYING_SELECTED_SETUP_OTHER ),
     m_ProductControllerStatePlayingSelectedSetupExiting( GetHsm(), &m_ProductControllerStatePlayingSelectedSetup, PRODUCT_CONTROLLER_STATE_PLAYING_SELECTED_SETUP_EXITING ),
     m_ProductControllerStateStoppingStreams( GetHsm(), &m_ProductControllerStateTop, PRODUCT_CONTROLLER_STATE_STOPPING_STREAMS ),
@@ -109,6 +110,7 @@ EddieProductController::EddieProductController( std::string const& ProductName )
     GetHsm().AddState( NotifiedNames_Name( NotifiedNames::SELECTED ), &m_ProductControllerStatePlayingSelectedNotSilent );
     GetHsm().AddState( NotifiedNames_Name( NotifiedNames::SELECTED ), &m_ProductControllerStatePlayingSelectedSetup );
     GetHsm().AddState( NotifiedNames_Name( NotifiedNames::SELECTED ), &m_ProductControllerStatePlayingSelectedSetupNetwork );
+    GetHsm().AddState( NotifiedNames_Name( NotifiedNames::SELECTED ), &m_ProductControllerStatePlayingSelectedSetupNetworkTransition );
     GetHsm().AddState( NotifiedNames_Name( NotifiedNames::SELECTED ), &m_ProductControllerStatePlayingSelectedSetupOther );
     GetHsm().AddState( NotifiedNames_Name( NotifiedNames::SELECTED ), &m_ProductControllerStatePlayingSelectedSetupExiting );
     GetHsm().AddState( "", &m_ProductControllerStateStoppingStreams );
@@ -479,6 +481,13 @@ void EddieProductController::HandleNetworkModuleReady( bool networkModuleReady )
     }
 
     m_isNetworkModuleReady = networkModuleReady;
+}
+
+void EddieProductController::RequestWiFiProfileStatus()
+{
+    AsyncCallback<NetManager::Protobuf::WiFiProfiles> networkWifiProfilesCb( std::bind( &EddieProductController::HandleWiFiProfileResponse ,
+                                                                             this, std::placeholders::_1 ), GetTask() );
+    m_FrontDoorClientIF->SendGet<NetManager::Protobuf::WiFiProfiles, EndPointsError::Error>( FRONTDOOR_NETWORK_WIFI_PROFILE_API, networkWifiProfilesCb, m_fdErrorCb );
 }
 
 bool EddieProductController::IsAllModuleReady() const
--- ProductController/EddieProductController.h
+++ ProductController/EddieProductController.h
@@ -40,6 +40,7 @@
 #include "ProductControllerStatePlayingSelectedSilent.h"
 #include "ProductControllerStatePlayingSelectedNotSilent.h"
 #include "ProductControllerStatePlayingSelectedSetup.h"
+#include "ProductControllerStatePlayingSelectedSetupNetworkTransition.h"
 #include "ProductControllerStatePlayingSelectedSetupNetwork.h"
 #include "ProductControllerStatePlayingSelectedSetupOther.h"
 #include "ProductControllerStatePlayingSelectedSetupExiting.h"
@@ -129,6 +130,7 @@ public:
         return false;
     }
 
+
     std::string const& GetProductType() const override;
     std::string const& GetProductModel() const override;
     std::string GetProductColor() const override;
@@ -136,6 +138,13 @@ public:
     std::string const& GetProductDescription() const override;
     std::string const& GetDefaultProductName() const override;
     BLESetupService::VariantId GetVariantId() const override;
+
+    void SetWifiProfileCount( uint32_t count ) override
+    {
+        m_wifiProfilesCount = count;
+    }
+
+    void RequestWiFiProfileStatus() override;
 
 private:
     /// Disable copies
@@ -387,22 +396,23 @@ private:
     ProductControllerStateNetworkStandbyNotConfigured   m_ProductControllerStateNetworkNotConfigured;
     ProductControllerStateFactoryDefault                    m_ProductControllerStateFactoryDefault;
 
-    ProductControllerStatePlayingDeselected                 m_ProductControllerStatePlayingDeselected;
-    ProductControllerStatePlayingSelected                   m_ProductControllerStatePlayingSelected;
-    ProductControllerStatePlayingSelectedSilent             m_ProductControllerStatePlayingSelectedSilent;
-    ProductControllerStatePlayingSelectedNotSilent          m_ProductControllerStatePlayingSelectedNotSilent;
-    ProductControllerStatePlayingSelectedSetup              m_ProductControllerStatePlayingSelectedSetup;
-    ProductControllerStatePlayingSelectedSetupNetwork       m_ProductControllerStatePlayingSelectedSetupNetwork;
-    ProductControllerStatePlayingSelectedSetupOther         m_ProductControllerStatePlayingSelectedSetupOther;
-    ProductControllerStatePlayingSelectedSetupExiting       m_ProductControllerStatePlayingSelectedSetupExiting;
-    ProductControllerStateStoppingStreams                   m_ProductControllerStateStoppingStreams;
-    ProductControllerStatePlayableTransition                m_ProductControllerStatePlayableTransition;
-    ProductControllerStatePlayableTransitionInternal        m_ProductControllerStatePlayableTransitionInternal;
-    ProductControllerStatePlayableTransitionIdle            m_ProductControllerStatePlayableTransitionIdle;
-    ProductControllerStatePlayableTransitionNetworkStandby  m_ProductControllerStatePlayableTransitionNetworkStandby;
-    ProductControllerStateSoftwareUpdateTransition          m_ProductControllerStateSoftwareUpdateTransition;
-    ProductControllerStatePlayingTransition                 m_ProductControllerStatePlayingTransition;
-    ProductControllerStatePlayingTransitionSelected         m_ProductControllerStatePlayingTransitionSelected;
+    ProductControllerStatePlayingDeselected                         m_ProductControllerStatePlayingDeselected;
+    ProductControllerStatePlayingSelected                           m_ProductControllerStatePlayingSelected;
+    ProductControllerStatePlayingSelectedSilent                     m_ProductControllerStatePlayingSelectedSilent;
+    ProductControllerStatePlayingSelectedNotSilent                  m_ProductControllerStatePlayingSelectedNotSilent;
+    ProductControllerStatePlayingSelectedSetup                      m_ProductControllerStatePlayingSelectedSetup;
+    ProductControllerStatePlayingSelectedSetupNetwork               m_ProductControllerStatePlayingSelectedSetupNetwork;
+    ProductControllerStatePlayingSelectedSetupNetworkTransition     m_ProductControllerStatePlayingSelectedSetupNetworkTransition;
+    ProductControllerStatePlayingSelectedSetupOther                 m_ProductControllerStatePlayingSelectedSetupOther;
+    ProductControllerStatePlayingSelectedSetupExiting               m_ProductControllerStatePlayingSelectedSetupExiting;
+    ProductControllerStateStoppingStreams                           m_ProductControllerStateStoppingStreams;
+    ProductControllerStatePlayableTransition                        m_ProductControllerStatePlayableTransition;
+    ProductControllerStatePlayableTransitionInternal                m_ProductControllerStatePlayableTransitionInternal;
+    ProductControllerStatePlayableTransitionIdle                    m_ProductControllerStatePlayableTransitionIdle;
+    ProductControllerStatePlayableTransitionNetworkStandby          m_ProductControllerStatePlayableTransitionNetworkStandby;
+    ProductControllerStateSoftwareUpdateTransition                  m_ProductControllerStateSoftwareUpdateTransition;
+    ProductControllerStatePlayingTransition                         m_ProductControllerStatePlayingTransition;
+    ProductControllerStatePlayingTransitionSelected                 m_ProductControllerStatePlayingTransitionSelected;
 
     /// Key Handler
     KeyHandlerUtil::KeyHandler                  m_KeyHandler;
