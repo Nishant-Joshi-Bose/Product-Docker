d2f67b95b44d7ed0a81004b6d9b09a8b06d465b0
Suhail-Ahmed-Randstad <Suhail_Ahmed@bose.com>
2018-03-14 18:38:19 -0400

    Removed extra parenthesis

--- ProductController/EddieProductController.cpp
+++ ProductController/EddieProductController.cpp
@@ -289,7 +289,7 @@ void EddieProductController::HandleNetworkStatus( const NetManager::Protobuf::Ne
 
 bool EddieProductController::IsNetworkConfigured() const
 {
-    return ( ( ( GetWifiProfileCount() ) &&
+    return ( ( GetWifiProfileCount() &&
                ( m_NetworkServiceUtil.GetNetManagerOperationMode() != NetManager::Protobuf::OperationalMode::wifiOff ) ) ||
              ( m_cachedStatus.get().isprimaryup() ) );
 }
