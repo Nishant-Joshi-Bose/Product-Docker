51e90458e019770c3d38c917f9a7fe2513f090b3
Chris Houston <Chris_Houston@bose.com>
2018-03-20 10:26:03 -0400

    -whitespace

--- ProductController/source/CustomStateMachine/CustomProductControllerStateAdaptIQ.h
+++ ProductController/source/CustomStateMachine/CustomProductControllerStateAdaptIQ.h
@@ -99,6 +99,7 @@ private:
 
     const uint32_t ADAPTIQ_TIMEOUT_MINUTES      = 20;
     const uint32_t ADAPTIQ_INACTIVITY_TIMEOUT   = 1 * ADAPTIQ_TIMEOUT_MINUTES * 60 * 1000;
+
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
