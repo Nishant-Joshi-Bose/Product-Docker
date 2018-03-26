f2a4ee630c98f09dfc67b8817229dcce6ae003b1
Karthik Sankar <Karthik_Sankar@bose.com>
2018-03-26 15:03:02 -0400

    Use display controller to turn OFF/ON display in LPS state

--- ProductController/CustomHsm/CustomProductControllerStateLowPowerStandby.cpp
+++ ProductController/CustomHsm/CustomProductControllerStateLowPowerStandby.cpp
@@ -9,7 +9,6 @@
 #include "ProductControllerHsm.h"
 #include "EddieProductController.h"
 #include "DPrint.h"
-#include "SystemUtils.h"
 
 static DPrint s_logger( "CustomProductControllerStateLowPowerStandby" );
 
@@ -28,25 +27,15 @@ void CustomProductControllerStateLowPowerStandby::HandleStateStart()
 {
     BOSE_INFO( s_logger, __func__ );
 
-    ///
     ///Turn OFF LCD display controller
-    ///TODO: Handshake with Display Controller module and perform this action
-    ///
-    BOSE_INFO( s_logger, "Disable LCD display controller TE_SYNC in %s.", __func__ );
-    SystemUtils::Spawn( { "echo -n 0 > /sys/devices/soc/7af6000.spi/spi_master/spi6/spi6.1/graphics/fb1/te" } );
-    BOSE_INFO( s_logger, "LCD display controller is turned OFF in %s.", __func__ );
-    SystemUtils::Spawn( { "/opt/Bose/webkit/target_scripts/lcd/turn-lcd-off" } );
+    BOSE_INFO( s_logger, "Turn LCD display OFF in %s.", __func__ );
+    GetCustomProductController().TurnDisplayOnOff( false );
 
     ProductControllerStateLowPowerStandby::HandleStateStart();
 
-    ///
     ///Turn ON LCD display controller
-    ///TODO: Handshake with Display Controller module and perform this action
-    ///
-    BOSE_INFO( s_logger, "LCD display controller is turned ON in %s.", __func__ );
-    SystemUtils::Spawn( { "/opt/Bose/webkit/target_scripts/lcd/turn-lcd-on" } );
-    BOSE_INFO( s_logger, "Enable LCD display controller TE_SYNC in %s.", __func__ );
-    SystemUtils::Spawn( { "echo -n 1 > /sys/devices/soc/7af6000.spi/spi_master/spi6/spi6.1/graphics/fb1/te" } );
+    BOSE_INFO( s_logger, "Turn LCD display ON in %s.", __func__ );
+    GetCustomProductController().TurnDisplayOnOff( true );
 }
 
 } /// namespace ProductApp
