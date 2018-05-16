18584340057974a32617a4935201be4f439f80c5
Aleksander Soltan <aleksander_soltan@bose.com>
2018-05-16 11:31:27 -0400

    Merge pull request #717 from BoseCorp/SOP_PGC-1765
    PGC-1765 Disconnecting a Bluetooth device from the Cheever turns off …

--- ProductController/source/ProductCecHelper.cpp
+++ ProductController/source/ProductCecHelper.cpp
@@ -473,38 +473,32 @@ void ProductCecHelper::HandleNowPlaying( const SoundTouchInterface::NowPlaying&
                                          nowPlayingStatus )
 {
     BOSE_DEBUG( s_logger, "CEC CAPS now playing status has been received." );
-    if( nowPlayingStatus.has_state( ) )
+    if( nowPlayingStatus.state( ).status( ) == SoundTouchInterface::Status::play )
     {
-        if( nowPlayingStatus.state( ).status( ) == SoundTouchInterface::Status::play )
+        if( nowPlayingStatus.has_container( )                          and
+            nowPlayingStatus.container( ).has_contentitem( )           and
+            nowPlayingStatus.container( ).contentitem( ).has_source( ) and
+            nowPlayingStatus.container( ).contentitem( ).has_sourceaccount( ) )
         {
-            if( nowPlayingStatus.has_container( )                          and
-                nowPlayingStatus.container( ).has_contentitem( )           and
-                nowPlayingStatus.container( ).contentitem( ).has_source( ) and
-                nowPlayingStatus.container( ).contentitem( ).has_sourceaccount( ) )
+            if( nowPlayingStatus.container( ).contentitem( ).source( ).compare( "PRODUCT" ) == 0   and
+                ( nowPlayingStatus.container( ).contentitem( ).sourceaccount( ).compare( "TV" ) == 0 or
+                  nowPlayingStatus.container( ).contentitem( ).sourceaccount( ).compare( "SLOT_0" ) == 0 or
+                  nowPlayingStatus.container( ).contentitem( ).sourceaccount( ).compare( "SLOT_1" ) == 0 or
+                  nowPlayingStatus.container( ).contentitem( ).sourceaccount( ).compare( "SLOT_2" ) == 0 ) )
             {
-                if( nowPlayingStatus.container( ).contentitem( ).source( ).compare( "PRODUCT" ) == 0   and
-                    nowPlayingStatus.container( ).contentitem( ).sourceaccount( ).compare( "TV" ) == 0 )
-                {
-                    BOSE_DEBUG( s_logger, "CEC CAPS now playing source is set to SOURCE_TV." );
+                BOSE_DEBUG( s_logger, "CEC CAPS now playing source is set to SOURCE_TV." );
 
-                    m_ProductLpmHardwareInterface->SendSourceSelection( LPM_IPC_SOURCE_TV );
-                }
-                else
-                {
-                    BOSE_DEBUG( s_logger, "CEC CAPS now playing source is set to LPM_IPC_SOURCE_INTERNAL." );
-
-                    m_ProductLpmHardwareInterface->SendSourceSelection( LPM_IPC_SOURCE_INTERNAL );
-                }
-
+                m_ProductLpmHardwareInterface->SendSourceSelection( LPM_IPC_SOURCE_TV );
             }
+            else
+            {
+                BOSE_DEBUG( s_logger, "CEC CAPS now playing source is set to LPM_IPC_SOURCE_INTERNAL." );
+
+                m_ProductLpmHardwareInterface->SendSourceSelection( LPM_IPC_SOURCE_INTERNAL );
+            }
+
         }
     }
-    else
-    {
-        BOSE_DEBUG( s_logger, "CEC CAPS now playing status is unknown. CEC STANDBY  %d",  LPM_IPC_SOURCE_STANDBY );
-        m_ProductLpmHardwareInterface->SendSourceSelection( LPM_IPC_SOURCE_STANDBY );
-    }
-
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -532,6 +526,7 @@ void ProductCecHelper::PowerOff( )
 
     msg.set_state( A4VVideoManagerServiceMessages::PowerState_t::PS_Low );
     m_CecHelper->SetPowerState( msg );
+    m_ProductLpmHardwareInterface->SendSourceSelection( LPM_IPC_SOURCE_STANDBY );
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
