228b10980c5138bce3daf84e7ccc8bdc4dc2b341
Nikhilesh Koshti <33822603+Nikhilesh-Koshti-eInfochips@users.noreply.github.com>
2018-05-18 22:57:58 +0530

    Update to minimize files and use common automation setup. (#704)

--- Test/LightBar/test_lightbar.py
+++ Test/LightBar/test_lightbar.py
@@ -20,20 +20,20 @@ import pytest
 
 from CastleTestUtils.LoggerUtils.CastleLogger import get_logger
 
-log = get_logger(__name__)
+LOGGER = get_logger(__name__)
 
 
-@pytest.mark.usefixtures('frontDoor')
-def test_get_active_animation(frontDoor):
+@pytest.mark.usefixtures('frontdoor_wlan')
+def test_get_active_animation(frontdoor_wlan):
     """
     Asserts that we receive active Lightbar states and transitions through the
     Front Door connection.
 
-    :param frontDoor: Fixture connection to the Front Door
+    :param frontdoor_wlan: Fixture connection to the Front Door
     :return: None
     """
-    lightbar_state = frontDoor.getActiveAnimation()
-    log.debug("The state is %s", lightbar_state)
+    lightbar_state = frontdoor_wlan.getActiveAnimation()
+    LOGGER.debug("LightBar state: %s", lightbar_state)
 
     transitions = lightbar_state["properties"]["supportedTransitions"]
     assert len(transitions) > 1, \
@@ -43,65 +43,65 @@ def test_get_active_animation(frontDoor):
     assert len(supported_animations) > 1, "Not able to find multiple animations in {}".format(supported_animations)
 
 
-@pytest.mark.usefixtures('frontDoor')
-def test_play_animation_in_loop(frontDoor):
+@pytest.mark.usefixtures('frontdoor_wlan')
+def test_play_animation_in_loop(frontdoor_wlan):
     """
     Queues all animations and verifies queue through Front Door connection.
 
-    :param frontDoor: Fixture connection to the Front Door
+    :param frontdoor_wlan: Fixture connection to the Front Door
     :return: None
     """
-    lightbar_state = frontDoor.getActiveAnimation()
-    log.debug("The state is %s", lightbar_state)
+    lightbar_state = frontdoor_wlan.getActiveAnimation()
+    LOGGER.debug("LightBar state: %s", lightbar_state)
     animations = lightbar_state["properties"]["supportedValues"]
-
     # Cycle through Supported Lightbar Animations
     for animation in animations:
         time.sleep(3)
-        log.debug("value = %s", animation)
+        LOGGER.debug("Current Animation: %s", animation)
 
         # create request to play animation in loop
         data = {"nextValue":
                     {"value": animation,
                      "transition": "SMOOTH",
                      "repeat": True}}
-        lightbar_state = frontDoor.playLightBarAnimation(json.dumps(data))
-        log.debug("lb_state = %s", lightbar_state)
-
-        next_animation = lightbar_state["nextValue"]["value"]
-        assert next_animation == animation, \
-            "Our next running animation ({}) was not stored in {}".format(animation, lightbar_state)
+        lightbar_state = frontdoor_wlan.playLightBarAnimation(json.dumps(data))
+        LOGGER.debug("LightBar State: %s", lightbar_state)
+        next_animation_response = lightbar_state.get("nextValue")
+        if next_animation_response:
+            next_animation = next_animation_response.get("value")
+            assert next_animation == animation, \
+                "Our next running animation ({}) was not stored in {}".format(animation, lightbar_state)
 
     # Finally stop any running animations
-    frontDoor.stopActiveAnimation(data)
+    frontdoor_wlan.stopActiveAnimation(json.dumps(data))
 
 
-@pytest.mark.usefixtures('frontDoor')
-def test_stop_active_animation(frontDoor):
+@pytest.mark.usefixtures('frontdoor_wlan')
+def test_stop_active_animation(frontdoor_wlan):
     """
     Attempts to stop a playing Animation after picking one from a random
     supported animation.
 
-    :param frontDoor: Fixture connection to the Front Door
+    :param frontdoor_wlan: Fixture connection to the Front Door
     :return: None
     """
-    # Adding a wait here b/c this test seemed to want to fail
+    # Adding a slight wait here because this test will fail
     # when run directly after test_playAnimationInLoop
     time.sleep(3)
-    lightbar_state = frontDoor.getActiveAnimation()
+    lightbar_state = frontdoor_wlan.getActiveAnimation()
     supported_animations = lightbar_state["properties"]["supportedValues"]
 
     total_animations = len(supported_animations)
     selected_animation = random.randint(0, total_animations - 1)
 
     animation_to_play = lightbar_state["properties"]["supportedValues"][selected_animation]
-    log.debug("Play animation - %s", animation_to_play)
+    LOGGER.debug("Play animation - %s", animation_to_play)
 
     data = {"nextValue":
                 {"value": animation_to_play,
                  "transition": "SMOOTH",
                  "repeat": True}}
-    lightbar_state = frontDoor.playLightBarAnimation(json.dumps(data))
+    lightbar_state = frontdoor_wlan.playLightBarAnimation(json.dumps(data))
 
     time.sleep(2)
 
@@ -109,30 +109,31 @@ def test_stop_active_animation(frontDoor):
                 {"value": animation_to_play,
                  "transition": "SMOOTH",
                  "repeat": True}}
-    lightbar_state = frontDoor.stopActiveAnimation(json.dumps(data))
+    lightbar_state = frontdoor_wlan.stopActiveAnimation(json.dumps(data))
 
-    # TODO: Cleanup this logic
-    if "currentValue" in lightbar_state:
+    # Ensure we have the correct LightBar state
+    if 'currentValue' in lightbar_state:
         if "value" in lightbar_state["currentValue"]:
             current_key = lightbar_state["currentValue"]["value"]
-            assert current_key == animation_to_play
+            assert current_key == animation_to_play, \
+                "Current Key ({}) does not match requested animation ({}).".format(current_key, animation_to_play)
         else:
-            assert True == False
+            assert False, "'value' not found in Current Value ({}).".format(lightbar_state['currentValue'])
     else:
-        assert True == False
+        assert False, "Key [{}] not found in ({}).".format('currentValue', lightbar_state)
 
     if "error" in lightbar_state:
         if "description" in lightbar_state["error"]:
             error = lightbar_state["error"]["description"]
-            assert error == ""
+            assert error == "", "Error Message found: {}".format(error)
 
 
-@pytest.mark.usefixtures('frontDoor')
-def test_invalid_animation_value(frontDoor):
+@pytest.mark.usefixtures('frontdoor_wlan')
+def test_invalid_animation_value(frontdoor_wlan):
     """
     Test to ensure that an improper animation does not play.
 
-    :param frontDoor: Fixture connection to the Front Door
+    :param frontdoor_wlan: Fixture connection to the Front Door
     :return: None
     """
     value = "SOME_ANIMATION"
@@ -142,7 +143,7 @@ def test_invalid_animation_value(frontDoor):
                  "repeat": True}}
 
     data = json.dumps(data)
-    lightbar_state = frontDoor.playLightBarAnimation(data)
+    lightbar_state = frontdoor_wlan.playLightBarAnimation(data)
 
     error_anticipation = "animation not supported"
     error = lightbar_state["error"]["description"]
