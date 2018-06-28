9f03b5c0abe224b8e82e648a65b78461d78cfe84
Sisi-Sun-Bose <Sisi_Sun@bose.com>
2018-06-28 10:30:27 -0400

    PGC_1926: change to work with common change

--- ProductController/source/CustomStateMachine/CustomProductControllerStateFirstBootGreetingTransition.cpp
+++ ProductController/source/CustomStateMachine/CustomProductControllerStateFirstBootGreetingTransition.cpp
@@ -28,7 +28,7 @@ void CustomProductControllerStateFirstBootGreetingTransition::HandleStateEnter()
     BOSE_INFO( s_logger, "The %s state is in %s.", GetName( ).c_str( ), __func__ );
     // LPM power has been requested to set to SYSTEM_STATE_ON in CustomBooting state
     // if it's already switched, trigger the callback again; otherwise just wait
-    if( GetCustomProductController( ).GetLpmSystemState() == SYSTEM_STATE_ON )
+    if( GetCustomProductController( ).GetLpmStatus().systemstate( ) == SYSTEM_STATE_ON )
     {
         ProductMessage productMessage;
         productMessage.mutable_lpmstatus( )->set_systemstate( SYSTEM_STATE_ON );
