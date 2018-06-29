cfe3b2ffd6abe18c7de625b34c5272b89dff32ad
Michael Cook <Michael_Cook@bose.com>
2018-06-29 17:22:28 -0400

    Rename Eddie... to Custom...
    Per email conversations.

---
+++ ProductController/CustomHsm/CustomProductControllerStateTop.cpp
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @file   CustomProductControllerStateTop.cpp
+/// @brief  The Top State in the Eddie Product.  Any methods getting
+///         called in this state indicate an unexpected call or an error.
+///
+/// Copyright 2017 Bose Corporation
+////////////////////////////////////////////////////////////////////////////////
+
+#include "CustomProductControllerStateTop.h"
+#include "ProductControllerHsm.h"
+#include "CustomProductController.h"
+#include "DPrint.h"
+
+static DPrint s_logger( "CustomProductControllerStateTop" );
+
+namespace ProductApp
+{
+CustomProductControllerStateTop::CustomProductControllerStateTop( ProductControllerHsm& hsm,
+                                                                  CHsmState* pSuperState,
+                                                                  Hsm::STATE stateId,
+                                                                  const std::string& name ) :
+    ProductControllerStateTop( hsm, pSuperState, stateId, name )
+{
+    BOSE_INFO( s_logger,  __func__ );
+}
+
+bool CustomProductControllerStateTop::HandleModulesReady()
+{
+    BOSE_INFO( s_logger, "%s, %d", __func__,  GetCustomProductController().IsAllModuleReady() );
+    return true;
+}
+
+bool CustomProductControllerStateTop::HandleIntentAuxIn( KeyHandlerUtil::ActionType_t intent )
+{
+    return true;
+}
+
+bool CustomProductControllerStateTop::HandleNowSelectionInfo( const SoundTouchInterface::NowSelectionInfo& info )
+{
+    BOSE_INFO( s_logger,  __func__ );
+    return true;
+}
+
+
+bool CustomProductControllerStateTop::HandleIntentCountDown( KeyHandlerUtil::ActionType_t intent )
+{
+    return true;
+}
+
+} // namespace ProductApp
