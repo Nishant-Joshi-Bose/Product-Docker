fa5e4860704924a47b2da59c88e3d6eee42592ee
Lagasse <jl1039542@pc-tests.bose.com>
2018-07-27 12:46:09 -0400

    Made new functions for negative tests, updated test script

--- test/ProductController/AudioSettings/AudioSettingsBase.py
+++ test/ProductController/AudioSettings/AudioSettingsBase.py
@@ -11,16 +11,13 @@ def setSource(frontDoor_conn, source, sourceAccount):
 	assert source_dict['body']['container']['contentItem']['source'] == source
 	assert source_dict['body']['container']['contentItem']['sourceAccount'] == sourceAccount
 
-#bass
+########## bass
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_bass(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	bass_dict = frontDoor_conn.setBassLevel(jsonMessage)
-	bass_dict = frontDoor_conn.getBassLevel()
-	time.sleep(1)
-	assert bass_dict['body']['persistence'] == persistence
-	assert bass_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_bass(frontDoor_conn, persistence, value):
@@ -29,16 +26,21 @@ def verify_bass(frontDoor_conn, persistence, value):
 	assert bass_dict['body']['persistence'] == persistence
 	assert bass_dict['body']['value'] == value
 
-#treble
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_bass_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	bass_dict = frontDoor_conn.setBassLevel(jsonMessage)
+	time.sleep(1)
+	assert bass_dict['error']['message'] == error_message
+
+########## treble
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_treble(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	treble_dict = frontDoor_conn.setTreble(jsonMessage)
-	treble_dict = frontDoor_conn.getTreble()
-	time.sleep(1)
-	assert treble_dict['body']['persistence'] == persistence
-	assert treble_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_treble(frontDoor_conn, persistence, value):
@@ -47,16 +49,21 @@ def verify_treble(frontDoor_conn, persistence, value):
 	assert treble_dict['body']['persistence'] == persistence
 	assert treble_dict['body']['value'] == value
 
-#center
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_treble_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	treble_dict = frontDoor_conn.setTreble(jsonMessage)
+	time.sleep(1)
+	assert treble_dict['error']['message'] == error_message
+
+########## center
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_center(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	center_dict = frontDoor_conn.setCenter(jsonMessage)
-	center_dict = frontDoor_conn.getCenter()
-	time.sleep(1)
-	assert center_dict['body']['persistence'] == persistence
-	assert center_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_center(frontDoor_conn, persistence, value):
@@ -65,16 +72,21 @@ def verify_center(frontDoor_conn, persistence, value):
 	assert center_dict['body']['persistence'] == persistence
 	assert center_dict['body']['value'] == value
 
-#surround
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_center_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	center_dict = frontDoor_conn.setCenter(jsonMessage)
+	time.sleep(1)
+	assert center_dict['error']['message'] == error_message
+
+########## surround
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_surround(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	surround_dict = frontDoor_conn.setSurround(jsonMessage)
-	surround_dict = frontDoor_conn.getSurround()
-	time.sleep(1)
-	assert surround_dict['body']['persistence'] == persistence
-	assert surround_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_surround(frontDoor_conn, persistence, value):
@@ -83,16 +95,21 @@ def verify_surround(frontDoor_conn, persistence, value):
 	assert surround_dict['body']['persistence'] == persistence
 	assert surround_dict['body']['value'] == value
 
-#avSync
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_surround_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	surround_dict = frontDoor_conn.setSurround(jsonMessage)
+	time.sleep(1)
+	assert surround_dict['error']['message'] == error_message
+
+########## avSync
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_avsync(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	avsync_dict = frontDoor_conn.setAVsync(jsonMessage)
-	avsync_dict = frontDoor_conn.getAVsync()
-	time.sleep(1)
-	assert avsync_dict['body']['persistence'] == persistence
-	assert avsync_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_avsync(frontDoor_conn, persistence, value):
@@ -101,16 +118,22 @@ def verify_avsync(frontDoor_conn, persistence, value):
 	assert avsync_dict['body']['persistence'] == persistence
 	assert avsync_dict['body']['value'] == value
 
-#gainOffset
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_avsync_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	avsync_dict = frontDoor_conn.setAVsync(jsonMessage)
+	time.sleep(1)
+	assert avsync_dict['error']['message'] == error_message
+
+########## gainOffset
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_gainOffset(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	gainOffset_dict = frontDoor_conn.setGainOffset(jsonMessage)
-	gainOffset_dict = frontDoor_conn.getGainOffset()
-	time.sleep(1)
-	assert gainOffset_dict['body']['persistence'] == persistence
-	assert gainOffset_dict['body']['value'] == value
+
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_gainOffset(frontDoor_conn, persistence, value):
@@ -119,16 +142,21 @@ def verify_gainOffset(frontDoor_conn, persistence, value):
 	assert gainOffset_dict['body']['persistence'] == persistence
 	assert gainOffset_dict['body']['value'] == value
 
-#subwooferGain
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_gainOffset_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	gainOffset_dict = frontDoor_conn.setGainOffset(jsonMessage)
+	time.sleep(1)
+	assert gainOffset_dict['error']['message'] == error_message
+
+########## subwooferGain
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_subwooferGain(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	subwooferGain_dict = frontDoor_conn.setSubwooferGain(jsonMessage)
-	subwooferGain_dict = frontDoor_conn.getSubwooferGain()
-	time.sleep(1)
-	assert subwooferGain_dict['body']['persistence'] == persistence
-	assert subwooferGain_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_subwooferGain(frontDoor_conn, persistence, value):
@@ -137,16 +165,21 @@ def verify_subwooferGain(frontDoor_conn, persistence, value):
 	assert subwooferGain_dict['body']['persistence'] == persistence
 	assert subwooferGain_dict['body']['value'] == value
 
-#surroundDelay
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_subwooferGain_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	subwooferGain_dict = frontDoor_conn.setSubwooferGain(jsonMessage)
+	time.sleep(1)
+	assert subwooferGain_dict['error']['message'] == error_message
+
+########## surroundDelay
+
 @pytest.mark.usefixtures("frontDoor_conn")
 def set_surroundDelay(frontDoor_conn, persistence, value):
 	message = {"persistence" : persistence,"value" : value}
 	jsonMessage = json.dumps(message, indent=4)
 	surroundDelay_dict = frontDoor_conn.setSurroundDelay(jsonMessage)
-	surroundDelay_dict = frontDoor_conn.getSurroundDelay()
-	time.sleep(1)
-	assert surroundDelay_dict['body']['persistence'] == persistence
-	assert surroundDelay_dict['body']['value'] == value
 
 @pytest.mark.usefixtures("frontDoor_conn")
 def verify_surroundDelay(frontDoor_conn, persistence, value):
@@ -154,3 +187,11 @@ def verify_surroundDelay(frontDoor_conn, persistence, value):
 	time.sleep(1)
 	assert surroundDelay_dict['body']['persistence'] == persistence
 	assert surroundDelay_dict['body']['value'] == value
+
+@pytest.mark.usefixtures("frontDoor_conn")
+def set_surroundDelay_error(frontDoor_conn, persistence, value, error_message):
+	message = {"persistence" : persistence,"value" : value}
+	jsonMessage = json.dumps(message, indent=4)
+	surroundDelay_dict = frontDoor_conn.setSurroundDelay(jsonMessage)
+	time.sleep(1)
+	assert surroundDelay_dict['error']['message'] == error_message
--- test/ProductController/run_tests.sh
+++ test/ProductController/run_tests.sh
@@ -1,7 +1,10 @@
+# make sure to have adb set up with permissions
+
 # Set up virtual environment
 virtualenv -p /usr/bin/python ./venv
 source ./venv/bin/activate
 pip install -r requirements.txt
 
 # Run tests
-pytest -sv AudioSettings/test_audio_settings.py --device-id 8857f8d --network-iface eth0 --target device --email professor_product_controller@bose.com --password 02bosebose
+pytest -svx AudioSettings/test_audio_settings.py --device-id 8857f8d --network-iface eth0 --target device --email jacqueline_lagasse123@bose.com --password 02bosebose
+pytest -svx AudioSettings/test_audio_settings_neg.py --device-id 8857f8d --network-iface eth0 --target device --email jacqueline_lagasse123@bose.com --password 02bosebose
