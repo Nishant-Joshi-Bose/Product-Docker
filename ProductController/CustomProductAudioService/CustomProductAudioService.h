ebf3e042f11837887e6236b1590fb54d355c1cd3
Sisi Sun <Sisi_Sun@bose.com>
2018-10-24 15:16:14 -0400

    PGC_3133_notify: reorder the function a little bit, not much

--- ProductController/CustomProductAudioService/CustomProductAudioService.h
+++ ProductController/CustomProductAudioService/CustomProductAudioService.h
@@ -69,19 +69,20 @@ private:
     /// APProduct handling functions
     /////////////////////////////////////////////////////////////////////////////////////////////////
     void RegisterAudioPathEvents() override;
-    void GetMainStreamAudioSettingsCallback( std::string contentItem,  const Callback<std::string, std::string> cb );
-    void SendAudioSettingsFrontDoorNotification() const;
+    void GetMainStreamAudioSettingsCallback( std::string contentItem,  const Callback<std::string, std::string> cb );  
     void SetStreamConfigCallback( std::vector<APProductCommon::ChannelParameters> channelParams, std::string serializedAudioSettings, std::string serializedInputRoute, const Callback<bool> cb );
     void InternalMuteCallback( bool mute );
     void RebroadcastLatencyCallback( uint32_t latency );
     void SendMainStreamAudioSettingsEvent();
     void ThermalDataReceivedCb( IpcSystemTemperatureData_t data );
 
+    void SendAudioSettingsFrontDoorNotification() const;
+    void SendAudioSettingsToDataCollection() const;
+
     /////////////////////////////////////////////////////////////////////////////////////////////////
     /// Helper functions to prepare m_MainStreamAudioSettings for APProduct to use
     /////////////////////////////////////////////////////////////////////////////////////////////////
     void FetchLatestAudioSettings();
-    void SendAudioSettingsToDataCollection() const;
     LpmServiceMessages::AudioSettingsAudioMode_t ModeNameToEnum( const std::string& modeName );
     LpmServiceMessages::AudioSettingsContent_t ContentTypeNameToEnum( const std::string& contentTypeName );
     LpmServiceMessages::AudioSettingsDualMonoMode_t DualMonoSelectNameToEnum( const std::string& dualMonoSelectName );
