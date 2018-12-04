ac82535f90582db99fe4425ea13a488c27261c80
Ann Clark <Ann_Clark@bose.com>
2018-12-04 08:26:28 -0500

    PGC-3417 Use uei key map pb file

--- ProductController/CustomProductKeyInputManager/CustomProductKeyInputManager.cpp
+++ ProductController/CustomProductKeyInputManager/CustomProductKeyInputManager.cpp
@@ -24,6 +24,7 @@
 #include "MonotonicClock.h"
 #include "AutoLpmServiceMessages.pb.h"
 #include "SystemSourcesProperties.pb.h"
+#include "UEIKeyNames.pb.h"
 
 using namespace ProductSTS;
 using namespace SystemSourcesProperties;
@@ -346,7 +347,8 @@ void CustomProductKeyInputManager::ExecutePowerMacro( const ProductPb::PowerMacr
 
     QSSMSG::IsKeyInCodesetMessage_t keyReq;
     keyReq.set_cicode( macroSrc->details().cicode() );
-    keyReq.set_ueikey( 3 );   // 3 is UEI kKey_POWER_OFF - These will be added to A4VQuickSetService protobuf file in next qss release
+//    keyReq.set_ueikey( 3 );   // 3 is UEI kKey_POWER_OFF - These will be added to A4VQuickSetService protobuf file in next qss release
+    keyReq.set_ueikey( UEIKeyNamesPB::ueikey::kKey_POWER_OFF );   // 3 is UEI kKey_POWER_OFF - These will be added to A4VQuickSetService protobuf file in next qss release
     m_QSSClient->IsKeyInCodeset( keyReq, srcCb );
 }
 
