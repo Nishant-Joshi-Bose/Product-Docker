b0f596ba2784f652f61a1e4a4b81fc46b6a6f86d
Ann Clark <Ann_Clark@bose.com>
2018-12-11 12:22:15 -0500

    Remove debug

--- ProductController/CustomProductKeyInputManager/CustomProductKeyInputManager.cpp
+++ ProductController/CustomProductKeyInputManager/CustomProductKeyInputManager.cpp
@@ -347,8 +347,7 @@ void CustomProductKeyInputManager::ExecutePowerMacro( const ProductPb::PowerMacr
 
     QSSMSG::IsKeyInCodesetMessage_t keyReq;
     keyReq.set_cicode( macroSrc->details().cicode() );
-//    keyReq.set_ueikey( 3 );   // 3 is UEI kKey_POWER_OFF - These will be added to A4VQuickSetService protobuf file in next qss release
-    keyReq.set_ueikey( UEIKeyNamesPB::ueikey::kKey_POWER_OFF );   // 3 is UEI kKey_POWER_OFF - These will be added to A4VQuickSetService protobuf file in next qss release
+    keyReq.set_ueikey( UEIKeyNamesPB::ueikey::kKey_POWER_OFF );
     m_QSSClient->IsKeyInCodeset( keyReq, srcCb );
 }
 
