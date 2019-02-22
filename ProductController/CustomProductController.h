06aaa2524c16b5c7d4935e3ac898c3c092a8ddfd
Robert Zampini <bob_zampini@bose.com>
2019-02-22 15:56:44 -0500

    Code changes resulting from code review feedback Made following changes: 1. Fix productName setting for non developemnt mode (Scott's catch) 2. For product config file problems (unable to read, product not found, etc.) call BOSE_DIE 3. Since we are now calling BOSE_DIE for a bad config file, no longer set defaults to Eddie settings. This also allowed me to clean up the code where the config file values are extracted.

--- ProductController/CustomProductController.h
+++ ProductController/CustomProductController.h
@@ -373,8 +373,8 @@ private:
     std::shared_ptr<DisplayController>                              m_displayController;
     IntentHandler                                                   m_IntentHandler;
     std::shared_ptr<Clock>                                          m_clock;
-    bool                                                            m_hasClock = true;
-    std::string                                                     m_productName = "HS 500";
+    bool                                                            m_hasClock = false;
+    std::string                                                     m_productName = "NOT SET";
 
     bool                                                            m_isBLEModuleReady  = false;
     bool                                                            m_isUiConnected = false;
