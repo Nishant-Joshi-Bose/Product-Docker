cb705b3dd63e95f00f17971268964c8cda7c0805
Sisi Sun <Sisi_Sun@bose.com>
2019-03-25 13:58:14 -0400

    remove the friendly name reporting in nowPlaying

--- ProductController/CustomSTSController/ProductSTSStateDeviceControl.cpp
+++ ProductController/CustomSTSController/ProductSTSStateDeviceControl.cpp
@@ -75,7 +75,7 @@ bool ProductSTSStateDeviceControl::HandleActivateRequest( const STS::Void & requ
                 m_np.set_seeksupported( false );
             }
 
-            m_np.mutable_contentitem()->set_name( source->details().friendlyname() );
+            //m_np.mutable_contentitem()->set_name( source->details().friendlyname() );
 
             m_np.set_playstatus( STS::PlayStatus::PLAY );
             STS::NowPlayingChange npc;
