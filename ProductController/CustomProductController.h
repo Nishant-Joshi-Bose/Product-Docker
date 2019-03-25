fad0ed3698361fc6cc8de7ed11f6686c6cde6e9d
Robert Zampini <bob_zampini@bose.com>
2019-03-25 12:32:09 -0400

    CASTLE-30049- Fix default product name (EddieClub) This commit fixes a race condition defining the default development product name. The problem was only exposed on an Eddie Club.  In our initial design for Eddie Club, we wanted to generalize the default development product name by putting it in the Product Config file.  The problem is that the Device Manager in the Common Product Controller calls the Custom Product Controller's GetDefaultProductname() during the "common initialize" phase of startup, which takes place BEFORE the product config file is read in (during the product dependent phase). I made an initial attempt to load the config file and extract the product name from the Custom Product Controller's constructor, but then one of the method's executed called GetProductType() which was not yet set! I could have taken the product type from Manufacturing data but what if someone in the future modified that code to call GetProductType() instead?  I decided to follow the design of Professor/Ginger Cheevers and simply hard code the development name directly in the GetDefaultProductname() method based on the product type obtained from manufacturing data.

--- ProductController/CustomProductController.h
+++ ProductController/CustomProductController.h
@@ -376,7 +376,6 @@ private:
     std::shared_ptr<DisplayController>                              m_displayController;
     IntentHandler                                                   m_IntentHandler;
     std::shared_ptr<Clock>                                          m_clock;
-    std::string                                                     m_productName = "NOT SET";
 
     bool                                                            m_isBLEModuleReady  = false;
     bool                                                            m_isUiConnected = false;
