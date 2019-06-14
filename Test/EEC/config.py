45b3ffffd87efdd576785962abc4fee2e50deee2
Jonathan-Liu-Bose <Jonathan_Liu@bose.com>
2019-06-14 11:56:05 -0400

    Cleaned up test folder

--- Test/EEC/config.py
+++
@@ -1,6 +0,0 @@
-''' Configuration file for the testcases.'''
-CONFIG = {
-    'network_interfaces_list_cmd'  : "ifconfig | sed 's/[ \t].*//;/^$/d'",
-    'network_standby_timeout' : 70, # seconds until device standby
-    'low_power_timeout' : 120, # seconds until device setup 2 * 60
-}
