1285c06be8a786696a163c3ff37aad53982ff1fd
Daniel Roberts <daniel_roberts@bose.com>
2019-12-20 11:26:09 -0500

    Add fixes to versioning and locking dependencies

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,42 @@
 from conans import python_requires
-import os
+import os, time, calendar
 from semver import SemVer
 
 common = python_requires('CastleConanRecipes/[>=0.0.22-0, include_prerelease=True]@BoseCorp/master')
 
 
 def get_version(git_tag):
-    if(git_tag is None):
-        # Sometimes this can happen when exporting sources, we already exported the version so we dont need to worry
-        return None
-    semver = SemVer(git_tag, loose=True)
-    build_info_delim = '.'
+    version_file = "version.txt"
+    if not os.path.exists(version_file):
+        print("Regenerating version.txt file")
+        if(git_tag is None):
+            # Sometimes this can happen when exporting sources, we already exported the version so we dont need to worry
+            return None
+        semver = SemVer(git_tag, loose=True)
+        build_info_delim = '.'
 
-    # formats the version to be something like 7.0.0.<timstamp>+<buildinfo> where build info will be something like
-    # gasdd.1234
-    if(os.getenv('CURR_TIME_MS') is None):
-        print("CURR_TIME_MS can not be null, please set this env variable")
-        raise Exception('Invalid version')
-    version = "%s.%s.%s-%s+%s" % (semver.major, semver.minor, semver.patch, os.getenv('CURR_TIME_MS'), build_info_delim.join(semver.build))
-    return version
+        # formats the version to be something like 7.0.0.<timestamp>+<buildinfo> where build info will be something like
+        # gasdd.1234
+        current_time = calendar.timegm(time.gmtime())
+        version = "%s.%s.%s-%s+%s" % (semver.major, semver.minor, semver.patch, current_time, build_info_delim.join(semver.build))
+
+        # write the version to a file
+        out_file = open(version_file, "w")
+        out_file.write(version)
+        out_file.close()
+
+        return version
+    else:
+        # read from the file if we arent trying to regenerate the version
+        file = open(version_file, "r")
+        version = file.read()
+        file.close()
+        return version
 
 
 class Professor(common.MakefilePackage):
     name = "Professor"
-    version = get_version(common.get_version_git_tag())
+    version = None
     generators = "json"
     url = "https://github.com/BoseCorp/Product-%s" % name
     scm = {
@@ -32,6 +45,8 @@ class Professor(common.MakefilePackage):
         "revision": "auto"
     }
 
+    exports = "depends.json", "dev_depends.json", "build_requires.json", "version.txt"
+
     options = {"product_package":[True, False], "ipks_list":"ANY"}
     default_options = {"product_package": False, "ipks_list":None}
 
@@ -39,6 +54,10 @@ class Professor(common.MakefilePackage):
     # for qc32 we need to EXCLUDE GVA
     qc8017_64_depends = ["GVA", "CastleSASS", "CastleAudioPathClient", "DinghyWebBrowser"]
     qc8017_32_excludes = ["GVA", "DinghyWebBrowser"]
+
+
+    def set_version(self):
+        self.version = get_version(common.get_version_git_tag())
 
 
     def build_requirements(self):
