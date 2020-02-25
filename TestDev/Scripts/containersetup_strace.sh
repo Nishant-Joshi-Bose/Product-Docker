efc01e771000997c71df71040183193d9a23aa0c
Peter Flanagan <peter_a_flanagan@bose.com>
2020-02-25 16:56:12 -0500

    Removed many files not used during productcontroller boot

---
+++ TestDev/Scripts/containersetup_strace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set up the virtual environment within the docker container and
+# use pytest to orchestrate the test
+# This script should be run from within the container
+# It will be located at /scratch/Scripts
+
+cd /scratch/CastleProducts/TestDev
+#rm -rf venv     #this is probably unnecessary but cleaner?
+virtualenv -p /usr/bin/python2.7 ./venv  --no-setuptools
+source venv/bin/activate
+rm -rf ~/.cache/pip
+./venv/bin/pip install setuptools==44.0.0
+make
+pip install -qr requirements.txt
+#pip install /scratch/Mock/CastleTestUtils/dist/*
+# The following definition is only needed for professor (and will not effect eddie)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(components get protobuf-x86_64 installed_location)/lib
+strace -o strace -ff pytest -sv test_start_product_controller.py
