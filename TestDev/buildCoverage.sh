09cbf254e5f1aa105b9e74c1744e1601b4191e4b
Peter Flanagan <peter_a_flanagan@bose.com>
2020-03-03 11:11:58 -0500

    Script to prepare for running product controller in a docker container

--- TestDev/buildCoverage.sh
+++ TestDev/buildCoverage.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# Build for Coverage. Use this after doing an x86_64 build
-#   The Coverage build allows you to run the CastleProducts component in a
-#   docker container
+# Build for running in a docker container
+# Currently must do an sdk=qc8017_32 build to create product_update.zip
+#   which contains the files which are placed in /opt/Bose/etc on target
+#   by Scripts/getFiles.sh.
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR="$THIS_DIR/../"
 cd $REPO_DIR
+
+make "$@" sdk=qc8017_32 cfg=Release deploy || exit
 make sdk=x86_64 cfg=Coverage
+TestDev/Scripts/getFiles.sh
 
