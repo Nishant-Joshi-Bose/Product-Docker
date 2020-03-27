fa7aa9f65d5ea7b96fcb310538dbee4643754b25
Peter Flanagan <peter_a_flanagan@bose.com>
2020-03-27 12:32:16 -0400

    Now runs with CastleTestUtils br Voice_req_fix
    Note I also added AudioSource for debugging.
    Had to ensure the Makefile and pytest.ini dir names agree

--- TestDev/Scripts/rundocker.sh
+++ TestDev/Scripts/rundocker.sh
@@ -13,4 +13,5 @@ docker run -it \
 -v /scratch/components-cache:/scratch/components-cache \
 -v $THIS_DIR:/scratch/Scripts \
 -v /scratch/CastleTestUtils:/scratch/CastleTestUtils \
+-v /scratch/AudioSource:/scratch/AudioSource \
 b0a9a495a8b5 bash
