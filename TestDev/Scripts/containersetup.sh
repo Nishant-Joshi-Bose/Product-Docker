82e9ace629521c6062bd168b12b555aac026b311
p-bhagia.Bose <punam_bhagia@bose.com>
2020-04-07 15:41:18 -0400

    added changes to run tests using python3

--- TestDev/Scripts/containersetup.sh
+++ TestDev/Scripts/containersetup.sh
@@ -7,13 +7,16 @@
 
 cd /scratch/CastleProducts/TestDev
 #rm -rf venv     #this is probably unnecessary but cleaner?
-virtualenv -p /usr/bin/python2.7 ./venv  --no-setuptools
-source venv/bin/activate
-rm -rf ~/.cache/pip
-./venv/bin/pip install setuptools==44.0.0
+#virtualenv -p /usr/bin/python2.7 ./venv  --no-setuptools --required for python2.7
+virtualenv -p /usr/bin/python3 ./venv3
+source venv3/bin/activate
+# rm -rf ~/.cache/pip -- commented for now
+#./venv/bin/pip install setuptools==44.0.0 -- required for python 2.7
 make
+pip install brainstem --trusted-host ingress-platform.live-aws-useast1.bose.io/dev/svc-core-devops-nexus/prod/core-devops-nexus-core --extra-index-url https://ingress-platform.live-aws-useast1.bose.io/dev/svc-core-devops-nexus/prod/core-devops-nexus-core/repository/pypi-all/simple
+pip install pyadb --trusted-host ingress-platform.live-aws-useast1.bose.io/dev/svc-core-devops-nexus/prod/core-devops-nexus-core --extra-index-url https://ingress-platform.live-aws-useast1.bose.io/dev/svc-core-devops-nexus/prod/core-devops-nexus-core/repository/pypi-all/simple
 pip install -qr requirements.txt
-#pip install /scratch/CastleTestUtils/dist/*
+
 # The following definition is only needed for professor (and will not effect eddie)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(components get protobuf-x86_64 installed_location)/lib
-pytest -sv test_start_product_controller.py
+./venv3/bin/python3 -m pytest -sv test_start_product_controller.py
